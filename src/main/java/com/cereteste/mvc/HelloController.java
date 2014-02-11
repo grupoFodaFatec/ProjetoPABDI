8afa2b7e8e9e4421d6eacc53a8c7f6cc1effa2bc
arthurmiranda2 <arthur_dimmi@hotmail.com>
2014-02-11 21:53:10 -0200

    - Alteração na classe HelloController.

--- src/main/java/com/cereteste/mvc/HelloController.java
+++ src/main/java/com/cereteste/mvc/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
     @RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
+		model.addAttribute("message", "Olá, mundo!");
 		return "hello";
 	}
 }
