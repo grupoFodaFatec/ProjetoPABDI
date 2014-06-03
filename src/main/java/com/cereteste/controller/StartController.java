7f24406bf278297f1c93938c24ab4a2415559d41
Glaucia Araujo <garaujo.torres@gmail.com>
2014-06-03 13:50:35 -0300

    Login para iniciar o jogo

--- src/main/java/com/cereteste/controller/StartController.java
+++ src/main/java/com/cereteste/controller/StartController.java
@@ -1,16 +1,25 @@
 package com.cereteste.controller;
 
+import com.cereteste.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.enterprise.context.SessionScoped;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/start")
 public class StartController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String startPage(ModelMap model) {
-        return "start";
+    @SessionScoped
+    public String startPage(ModelMap model, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if(user != null){
+            return "start";
+        }
+        return "redirect:/index";
     }
 }
